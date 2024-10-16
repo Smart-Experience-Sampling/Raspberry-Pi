3ef7bdc4c6446358c73f51b9c49cb49ad1d3e2ae
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2024-10-16 23:45:43 +0200

    Bluetooth-server: Very BASIC server.py should work

--- bluetooth-server/server.py
+++ bluetooth-server/server.py
@@ -2,4 +2,23 @@ import socket
 from getmac import get_mac_address as gma
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-print(gma())
+mac = gma()
+
+server.bind((mac, 4))
+
+server.listen(1)
+client, addr = server.accept()
+
+try:
+    while True:
+        data = client.recv()
+        if not data:
+            break
+        print(f"Message: {data.decode('utf-8')}")
+        message = input("Enter message: ")
+        client.send(message.encode("utf-8"))
+except OSError as e:
+    print(f"Error: {e}")
+
+client.close()
+server.close()
