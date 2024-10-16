f1ecd6ab40085e43b6595cbc79cdee70d3223f5e
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2024-10-16 23:49:26 +0200

    Bluetooth-server: Init client.py should work

---
+++ bluetooth-server/client.py
@@ -0,0 +1,19 @@
+import socket
+
+client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+mac = input("Enter mac address: ")
+client.connect(mac)
+
+
+try:
+    while True:
+        message = input("Enter message: ")
+        client.send(message.encode("utf-8"))
+        data = client.recv(1024)
+        if not data:
+            break
+
+        print(f"Mesage: {data.decode('utf-8')}")
+
+except OSError as e:
+    print(f"Error: {e}")
